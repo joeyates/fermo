5a04284f9105ecf03c29bb77f6d7a9b7fe50c185
Joe Yates <joe.g.yates@gmail.com>
2025-07-23 14:32:36 +0200

    Allow clients in debug mode to force a page reload

--- lib/fermo/live/socket.ex
+++ lib/fermo/live/socket.ex
@@ -1,6 +1,11 @@
 defmodule Fermo.Live.Socket do
   @behaviour :cowboy_websocket
 
+  alias Fermo.Live.Dependencies
+  alias Fermo.Live.SocketRegistry
+
+  require Logger
+
   def init(request, _state) do
     {:cowboy_websocket, request, []}
   end
@@ -21,6 +26,26 @@ defmodule Fermo.Live.Socket do
     Fermo.Live.SocketRegistry.subscribe(path, self())
 
     {:reply, {:text, "fermo:live-reload subscribed for '#{path}'"}, state}
+  end
+
+  def websocket_handle({:text, "rebuild:" <> path}, state) do
+    Logger.debug("#{__MODULE__} - Handling rebuild request, path: #{path}")
+
+    response =
+      case Fermo.Live.Dependencies.page_from_path(path) do
+        {:ok, page} ->
+          # TODO: It's very heavy-handed to reload the *whole* configuration
+          #   It would be better to have a method to reload just one page
+          {:ok} = Dependencies.reinitialize()
+          {:ok} = SocketRegistry.reload(page.path)
+          "Initiated rebuild of '#{path}'"
+
+        {:error, :not_found} ->
+          Logger.error("#{__MODULE__} - Page not found for rebuild request, path: #{path}")
+          "Page not found for path: #{path}"
+      end
+
+    {:reply, {:text, response}, state}
   end
 
   def websocket_handle(_info, state) do
