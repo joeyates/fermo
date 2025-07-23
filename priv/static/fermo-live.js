3863b09fa6c3ad1ee110be28fd2e2d570c1702f2
Joe Yates <joe.g.yates@gmail.com>
2025-07-23 14:33:37 +0200

    Namespace logging messages

--- priv/static/fermo-live.js
+++ priv/static/fermo-live.js
@@ -1,4 +1,6 @@
 const FERMO_LIVE_SOCKET = (location => {
+  console.debug('fermo-live: initializing WebSocket connection, location: ', location)
+
   const PING_INTERVAL = 10000 // 10 seconds
 
   const protocol = location.protocol == 'https:' ? 'wss' : 'ws'
@@ -11,36 +13,37 @@ const FERMO_LIVE_SOCKET = (location => {
   }
 
   socket.onopen = e => {
-    console.log('socket onopen')
+    console.debug('fermo-live: socket onopen')
     socket.send('subscribe:live-reload:' + window.location.pathname)
+
     pingTimer = window.setInterval(() => {
       socket.send(JSON.stringify({event: 'ping'}))
     }, PING_INTERVAL)
   }
 
   socket.onmessage = (event) => {
-    console.log('onmessage: ', event)
+    console.debug('fermo-live: onmessage: ', event)
     if (event.data === 'reload') {
       reloadPage()
     }
   }
 
   socket.onclose = event => {
-    // Clean up ping timer
     if(pingTimer) {
       window.clearInterval(pingTimer)
       pingTimer = null
     }
+
     if (event.wasClean) {
-      console.log('onclose clean event:', event)
+      console.debug('fermo-live: onclose clean event:', event)
     } else {
       // TODO: Poll to try to reconnect
-      console.log('onclose died event:', event)
+      console.debug('fermo-live: onclose non-clean event:', event)
     }
   }
 
   socket.onerror = error => {
-    console.log('onerror error:', error)
+    console.error('fermo-live: onerror error:', error)
   }
 
   return socket
